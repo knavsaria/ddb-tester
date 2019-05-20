c957162f2aeb4df0929d8541722debcfddb34a32
EC2 Default User <ec2-user@ip-172-31-88-241.ec2.internal>
2019-05-20 09:49:42 +0000

    corrected timing

--- ddbtest.sh
+++ ddbtest.sh
@@ -32,7 +32,7 @@ poll_describe_table() {
 }
 	
 #create table, then wait until it is created (using built it 'cli' wait feature)
-
+echo "Starting table $table_name creation..."
 aws dynamodb create-table --table-name $table_name --attribute-definitions file://table_create_defs --key-schema file://table_create_key_schema --global-secondary-indexes file://table_create_gsi --provisioned-throughput file://table_create_throughput
 
 echo "Create table successfull. Waiting for it to become ACTIVE"
@@ -44,31 +44,34 @@ echo "Time taken: $(($create_duration / 60)) minutes and $(($create_duration % 6
 
 #update table, then wait until it is active (using custom poll_describe_table function)
 #update 1
+echo "Starting update 1..."
 aws dynamodb update-table --table-name $table_name --attribute-definitions file://table_update_defs --global-secondary-index-updates file://table_update_gsi_1 --billing-mode PAY_PER_REQUEST
 
 poll_describe_table
 
-update_1_duration=$SECONDS
+let update_1_duration=$SECONDS-$create_duration
 echo "Table update 1 complete"
 echo "Time taken: $(($update_1_duration / 60)) minutes and $(($update_1_duration % 60)) seconds"
 
 #update 2
+echo "Starting update 2..."
 aws dynamodb update-table --table-name $table_name --attribute-definitions file://table_update_defs --global-secondary-index-updates file://table_update_gsi_2
 
 poll_describe_table
 
-update_2_duration=$SECONDS
+let update_2_duration=$SECONDS-$create_duration-$update_1_duration
 echo "Table update 2 complete"
 echo "Time taken: $(($update_2_duration / 60)) minutes and $(($update_2_duration % 60)) seconds"
 
 #delete table, then wait until it is deleted (using built in cli 'wait' feature
+echo "Starting table $table_name delete...
 aws dynamodb delete-table --table-name $table_name
 aws dynamodb wait table-not-exists --table-name $table_name
-delete_duration=$SECONDS
+let delete_duration=$SECONDS-$create_duration-$update_1_duration-$update_2_duration
 echo "Table delete complete"
 echo "Time taken: $(($delete_duration / 60)) minutes and $(($delete_duration % 60)) seconds"
 
 echo "================================================================="
-let total_test_time=$create_duration+$update_1_duration+$update_2_duration+$delete_duration
+let total_test_time=$SECONDS
 echo "TOTAL TIME FOR TEST: $((total_test_time / 60)) minutes and $((total_test_time % 60)) seconds"
 exit 0
